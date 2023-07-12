652d6d30c9f12e03f2ce144c837bc18377e52328
Mateusz <m.zaniewski1995@gmail.com>
2023-07-12 23:08:06 +0200

    import Login and Registration component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-// import { Login } from './components/login';
-// import { Registration } from './components/registration';
+import { Login } from './components/Login';
+import { Registration } from './components/Registration';
 import './index.css';
 import './style.css';
 import { useState } from 'react';
@@ -7,7 +7,7 @@ function App() {
 
 const [slider, setSlider] = useState<boolean>(false)
   return (
-    <body className={`body ${slider == false ? "" : "slide"}`}>
+    <div className={`wrapper ${slider == false ? "" : "slide"}`}>
       <main className="container" >
         <div className="box signin">
           <h2 className="header">Already have an account ?</h2>
@@ -18,35 +18,11 @@ const [slider, setSlider] = useState<boolean>(false)
           <button className="button" onClick={()=>setSlider(true)} >Sign up</button>
         </div>
         <div className="formWrapper">
-
-
-            {/* LOGOWANIE */}
-          <div className='form signInForm'>
-            <form>
-              <h3>Sign in</h3>
-              <input type="text" placeholder="Username"/>
-              <input type="password" placeholder="Password"/>
-              <button className='formBtn' >Login</button>
-              <a href="#" className="forgot">Forgot Password</a>
-            </form>
-          </div>
-        {/* <Login/> */}
-          {/* REJESTRACJA */}
-          <div className='form signUpForm'>
-            <form>
-              <h3>Sign Up</h3>
-              <input type="text" placeholder="Username"/>
-              <input type="email" placeholder="Email"/>
-              <input type="password" placeholder="Password"/>
-              <input type="password" placeholder="Confirm Password"/>
-              <button className='formBtn' >Register</button>
-              <a href="#" className="forgot">Forgot Password</a>
-            </form>
-          </div>
-          {/* <Registration/> */}
+          <Login/>
+          <Registration/>
         </div>
       </main>
-    </body>
+    </div>
   )
 }
 
