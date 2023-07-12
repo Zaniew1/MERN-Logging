2453e806570a81fe65af548aecd587486dfe60c3
Mateusz <m.zaniewski1995@gmail.com>
2023-07-12 23:08:50 +0200

    add loginUser route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.route('/getAllUsers', ).get(userController.getAllUsers);
 router.route('/createNewUser', ).post(userController.createNewUser);
+router.route('/LoginUser', ).post(userController.loginUser);
 
 module.exports = router;
