60e1f0003ef30c2055079764c440976e5e19a351
Mateusz <m.zaniewski1995@gmail.com>
2023-07-12 23:08:37 +0200

    add loginUser controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ const Users = require('../models/userModel');
 exports.getAllUsers = async (req, res) => {
     try{
         const allUsers = await Users.find();
-        console.log(allUsers);
+        console.log(allUsers)
         res.status(200).json({
             status:'succes',
             results: allUsers.length,
@@ -16,13 +16,34 @@ exports.getAllUsers = async (req, res) => {
         console.log(err)
     }
 }
+
+exports.loginUser = (req, res)=>{
+    const {email, password} = req.body;
+    console.log(email)
+    console.log(password)
+
+}
+
 exports.createNewUser = async (req, res) => {
     try{
-        
-        const user = req.body;
-        console.log(user)
-        const newUser = new Users(user);
-        await newUser.save();
+        const {username, email, password, confirmPassword} = req.body;
+        if(password !== confirmPassword){
+            console.log('zjebałeś')
+        }
+        else{
+            const newUser = new Users({
+                username, 
+                email,
+                password,
+                confirmPassword
+            });
+            await newUser.save();
+            res.status(200).json({
+                status:'succes',
+                newUser
+            })
+
+        }
     }catch(err){
         console.log(err)
     }
