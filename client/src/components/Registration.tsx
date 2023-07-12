06f01cd257d40e16c53b26648e3e50eab3e38d5d
Mateusz <m.zaniewski1995@gmail.com>
2023-07-12 23:07:30 +0200

    add error handling and add fetch request

--- client/src/components/Registration.tsx
+++ client/src/components/Registration.tsx
@@ -1,45 +1,57 @@
 import {  useState } from 'react';
+import '../index.css';
 import '../style.css';
 export  const Registration = () => {
 
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [confirmPassword, setConfirmPassword] = useState<string>('');
-    const [username, setUsername] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [error, setError] = useState<string>("")
     function createNewUser(e: React.SyntheticEvent<EventTarget>): void {
+      console.log(username)
       e.preventDefault();
-    //   console.log("ok");
-    //     fetch("http://localhost:3001/createNewUser", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({ email, password }),
-    //     })
-    //     .then((response) => {
-    //       if (!response.ok) {
-    //         throw new Error("Network response was not ok");
-    //       }
-    //       return response.json();
-    //     })
-    //     .then((result) => {
-    //       console.log(result);
-    //     })
-    //     .catch((error) => {
-    //       console.error("Error:", error);
-    //     });
+      if(password !== confirmPassword ){
+        setError("Hasła powinne być takie same");
+        return;
+      }
+      if(!password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)){
+        setError("Hasło powinno zawierać minimum 8 znaków, 1 dużą literę, 1 małą i jeden znak specjalny");
+        return;
+      }
+        
+        fetch("http://localhost:3001/createNewUser", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, email, password, confirmPassword }),
+        })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          setError("Błąd serwera, spróbuj później");
+        });
     }
 
     return(
         <div className='form signUpForm'>
             <form>
               <h3>Sign Up</h3>
-              <input type="text" placeholder="Username" onChange={(e)=>{setUsername(e.target.value)}} value={username}/>
-              <input type="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} value={email}/>
-              <input type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} value={password}/>
-              <input type="password" placeholder="Confirm Password" onChange={(e)=>{setConfirmPassword(e.target.value)}} value={confirmPassword}/>
-              <button onSubmit={createNewUser} className='formBtn' >Register</button>
-              <a href="#" className="forgot">Forgot Password</a>
+              <input type="text" placeholder="Username" onChange={(e)=>{setUsername(e.target.value)}} value={username} required/>
+              <input type="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} value={email} required/>
+              <input type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value); setError('')}} value={password} required/>
+              <input type="password" placeholder="Confirm Password" onChange={(e)=>{setConfirmPassword(e.target.value);setError('')}} value={confirmPassword} required/>
+              <button onClick={createNewUser} className='formBtn' >Register</button>
+              <div className={error ? "error" : ""}>{error ? error : ''}</div>
             </form>
         </div>
     );
--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,19 +1,48 @@
 import {  useState } from 'react';
+import '../index.css';
 import '../style.css';
 export  const Login= () => {
 
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-
+    const [error, setError] = useState<string>("")
+    function LoginUser(e: React.SyntheticEvent<EventTarget>): void{
+      e.preventDefault();
+      console.log(email)
+      if(!password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)){
+        setError("Hasło powinno zawierać minimum 8 znaków, 1 dużą literę, 1 małą i jeden znak specjalny");
+        return;
+      }
+      fetch("http://localhost:3001/LoginUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
     return(
       <div className='form signInForm'>
       <form>
         <h3>Sign in</h3>
-        <input type="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} value={email}/>
-        <input type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} value={password}/>
-        <button className='formBtn'  >Login</button>
+        <input type="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} value={email} required/>
+        <input type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} value={password} required/>
+        <button className='formBtn' onClick={LoginUser} >Login</button>
         <a href="#" className="forgot">Forgot Password</a>
+        <div className={error ? "error" : ""}>{error ? error : ''}</div>
       </form>
     </div>
     );
